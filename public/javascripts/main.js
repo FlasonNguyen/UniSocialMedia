a534abfe3a7b84a467da94ba9bfe7e126b34fc39
phungo <45889218+NgoThanhPhu@users.noreply.github.com>
2021-05-16 09:42:47 +0700

    update date

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -425,3 +425,11 @@ function sendMessage() {
 //     document.getElementById('popupdiv').style.display = "block"
 //     console.log('showed')
 // }
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+today = 'Hôm nay là ngày ' + dd + ' tháng ' + mm + ' năm ' + yyyy + '!';
+document.getElementById('date').innerHTML = today;
