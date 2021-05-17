ea5ca76baa7367eda630f36104e7d13c79e67d60
phungo <45889218+NgoThanhPhu@users.noreply.github.com>
2021-05-17 23:34:42 +0700

    Update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,7 +11,7 @@ tinymce.init({
     plugins: [
     'advlist autolink lists link image charmap print preview anchor',
     'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table paste code help wordcount'
+    'insertdatetime media table paste code help'
     ],
     toolbar: 'undo redo | formatselect | ' +
     'bold italic backcolor | alignleft aligncenter ' +
