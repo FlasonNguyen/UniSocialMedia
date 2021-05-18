bd9d09729441908ba48984672c2a39a2edf00601
phungo <45889218+NgoThanhPhu@users.noreply.github.com>
2021-05-18 10:49:07 +0700

    update welcome

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -437,3 +437,22 @@ function sendMessage() {
 //     console.log('showed')
 // }
 
+function change_left() {
+    $('div').removeClass('slide-right').addClass('slide-left');
+}
+
+function change_right() {
+    $('div').removeClass('slide-left').addClass('slide-right');
+    change_title();
+}
+
+function to_left() {
+setInterval(change_left, 10000);
+};
+
+function to_right() {
+    setInterval(change_right, 20000);
+};
+
+to_left();
+to_right();
